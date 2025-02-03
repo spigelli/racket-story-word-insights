9dd2d9a4afaddc80071860de888ef43e3241ae00
Isaac <isaacspiegel@gmail.com>
2025-02-03 14:20:12 -0600

    Prompt for files

--- src/main.rkt
+++ src/main.rkt
@@ -1,6 +1,6 @@
 #lang racket
 
-(define (readFiles)
+(define (read-files)
   (define filesDirPath (
     simplify-path (
       build-path
@@ -10,8 +10,30 @@
   (directory-list filesDirPath)
 )
 
+(define (get-file-selection fileNames)
+  ; clear the screen
+  (system "clear")
+  (displayln "Select a file to read:")
+  (for-each (lambda (fileName index)
+    (displayln (string-append (number->string index) ". " (path->string fileName)))
+  ) fileNames (range (length fileNames)))
+  (display "Enter the number of the file you want to read: ")
+  (let ([fileIndex (read)])
+    (if (and (integer? fileIndex) (<= 0 fileIndex) (< fileIndex (length fileNames)))
+      (list-ref fileNames fileIndex)
+      (begin
+        (displayln "Invalid selection. Please try again.")
+        (get-file-selection fileNames)
+      )
+    )
+  )
+)
+
+
+
 (define (main)
-  (displayln (readFiles))
+  ; (displayln (readFiles))
+  (displayln (get-file-selection (read-files)))
 )
 
 (main)

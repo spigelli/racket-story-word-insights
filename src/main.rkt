dc829493a170b1e051a1e91de858d5b50bb83862
Isaac <isaacspiegel@gmail.com>
2025-02-05 10:50:10 -0600

    Select file path

--- src/main.rkt
+++ src/main.rkt
@@ -1,15 +1,6 @@
 #lang racket
 
-(define (read-files)
-  (define filesDirPath (
-    simplify-path (
-      build-path
-        (path->directory-path (find-system-path 'run-file)) 'up 'up "files"
-    )
-  ))
-  (directory-list filesDirPath)
-)
-
+; Takes a list of file names and returns the file name selected by the user
 (define (get-file-selection fileNames)
   ; clear the screen
   (system "clear")
@@ -29,11 +20,40 @@
   )
 )
 
+; Takes the list of file names and the directory path where the files are located
+(define (build-file-map fileNames filesDirPath)
+  (define mapEntries (
+    map ( lambda (fileName)
+      (list fileName (build-path filesDirPath fileName))
+    ) fileNames
+  ))
 
+  (make-hash mapEntries)
+)
+
+; Takes the file name and the hash map of file names to file paths and returns the file path
+(define (get-file-path fileName filePathsByName)
+  (first (hash-ref filePathsByName fileName))
+)
+
+; Reads the file at provided path and prints the contents
+(define (read-file filePath)
+  (displayln "TODO...")
+)
 
 (define (main)
-  ; (displayln (readFiles))
-  (displayln (get-file-selection (read-files)))
+  (define filesDirPath (
+    simplify-path (
+      build-path
+        (path->directory-path (find-system-path 'run-file)) 'up 'up "files"
+    )
+  ))
+  (define fileNames (directory-list filesDirPath))
+  (define filePathsByName (build-file-map fileNames filesDirPath))
+  (define selection (get-file-selection fileNames))
+  (define filePath (get-file-path selection filePathsByName))
+  (displayln (string-append "Reading file: " (path->string filePath)))
+  ; (read-file filePath)
 )
 
 (main)
